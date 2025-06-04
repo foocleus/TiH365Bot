7954cf1970a596fda96d37454ee412c1eaa5cc63
Makzzzimus <52410386+Makzzzimus@users.noreply.github.com>
2025-06-04 15:19:52 +0300

    fix: Clarify sentences

--- stores/CommandStore.py
+++ stores/CommandStore.py
@@ -8,10 +8,10 @@ EVENTSTODAY = BotCommand(command="eventstoday", description=Strs.COM_EVENTSTODAY
 EVENTSTHATDAY = BotCommand(command="eventsthatday", description=Strs.COM_EVENTSTHATDAY)
 PREFERENCES = BotCommand(command="preferences", description=Strs.COM_PREFERENCES)
 PREF = BotCommand(command="pref", description=Strs.COM_PREFERENCES)
-LANGUAGE = BotCommand(command="language", description=Strs.COM_PREFERENCES)
-SECTIONS = BotCommand(command="sections", description=Strs.COM_PREFERENCES)
-ENTRIES = BotCommand(command="entries", description=Strs.COM_PREFERENCES)
-SCHEDULE = BotCommand(command="schedule", description=Strs.COM_PREFERENCES)
+LANGUAGE = BotCommand(command="language", description=Strs.COM_LANGUAGE)
+SECTIONS = BotCommand(command="sections", description=Strs.COM_SECTIONS)
+ENTRIES = BotCommand(command="entries", description=Strs.COM_ENTRIES)
+SCHEDULE = BotCommand(command="schedule", description=Strs.COM_SCHEDULE)
 
 __copy = locals().copy()
 
