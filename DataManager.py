5e580b1e1423497b0ddc4fa417fab93b94bf3914
Makzzzimus <52410386+Makzzzimus@users.noreply.github.com>
2025-06-05 19:47:11 +0300

    feat: Changed default 'scheduledHour'

--- DataManager.py
+++ DataManager.py
@@ -9,7 +9,7 @@ from WikiParser import EVENTS, BIRTHS, DEATHS, HOLIDAYS
 defaultValues = {
     "lang": "en",
     "isActivated": False,
-    "scheduledHour" : 9,
+    "scheduledHour" : 12,
     "selectedSections": [EVENTS, BIRTHS, DEATHS, HOLIDAYS],
     "entriesPerRange": [3, 3, 3],
     "holidaysEntries": 5,
